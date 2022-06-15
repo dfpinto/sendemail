75b23c0fc3aab738c184bd2afd5c908c5ff39945
dfpinto <dirley.figueredo@gmail.com>
2022-06-14 21:32:14 -0300

    Sending email

--- src/main/java/com/devsuperior/integrations/controllers/EmailController.java
+++ src/main/java/com/devsuperior/integrations/controllers/EmailController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.integrations.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.integrations.dto.EmailDTO;
+import com.devsuperior.integrations.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailController {
 	
+	@Autowired
+	private EmailService emailService;
+	
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+		emailService.send(dto);
 		return ResponseEntity.noContent().build();
 	}
 
